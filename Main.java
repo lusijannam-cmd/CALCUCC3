2a1b2e4560c3e9777e4faaf36a4432ad9e025724
lusii <lusijannam@gmail.com>
2026-01-29 23:00:16 +0800

    main

---
+++ Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("MDS Calculator");
+        System.out.println("1. Multiply");
+        System.out.println("2. Divide");
+        System.out.println("3. Subtract");
+        System.out.print("Choose (1-3): ");
+
+        int choice = sc.nextInt();
+
+        System.out.print("Enter first number: ");
+        double a = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double b = sc.nextDouble();
+
+        switch (choice) {
+            case 1:
+                System.out.println("Result: " + (a * b));
+                break;
+            case 2:
+                System.out.println("Result: " + (a / b));
+                break;
+            case 3:
+                System.out.println("Result: " + (a - b));
+                break;
+            default:
+                System.out.println("Invalid choice!");
+        }
+        sc.close();
+    }
+}
+
